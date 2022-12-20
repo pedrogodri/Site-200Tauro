585182591e4c4eadf816de8dc45f31233f24dfc3
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-20 19:28:32 -0300

    Commit Inicial

---
+++ src/Devs2Blu.ProjetosAula.200Tauro.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devs2Blu.ProjetosAula._200Tauro.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
